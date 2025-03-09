05132db5234579f8cb4715595f323630cfc0a244
SanjanaKrishna <32275576+SanjanaKrishna@users.noreply.github.com>
2025-03-09 21:46:46 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-sanjana"
+    bucket         = "terraform-jenkins-sanjana"
     key            = "remote.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
