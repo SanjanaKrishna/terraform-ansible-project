9b12cd4bde0204f9fe96e443919afbee1362be84
SanjanaKrishna <32275576+SanjanaKrishna@users.noreply.github.com>
2025-03-10 00:24:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,14 @@ module "ec2" {
   source        = "./modules/ec2-instance"
   instance_type = var.instance_type
   ami_id        = var.ami_id
-  #key_name      = aws_key_pair.ssh_key.key_name
+  key_name      = aws_key_pair.ssh_key.key_name
   subnet_id     = var.subnet_id
 }
 
-#resource "aws_key_pair" "ssh_key" {
- # key_name   = "terraform-key"
- # public_key = file("~/.ssh/id_rsa.pub")
-# }
+resource "aws_key_pair" "ssh_key" {
+ key_name   = "terraform-key"
+ public_key = file("~/.ssh/id_rsa.pub")
+}
 
 output "instance_public_ip" {
   value = module.ec2.public_ip
